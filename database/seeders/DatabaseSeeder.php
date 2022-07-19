870cbb5a6367dc528a129304bfaa6925f7f7f03c
B <henryirons@yahoo.com>
2022-07-19 04:29:36 +0100

    created ListingFactory for the Listing model to seed the database apparently the naming convention for the factory is ModelnameFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
+use Database\Factories\ListingsFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,27 +17,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          \App\Models\User::factory(8)->create();
-         Listing::create([
-             'title'=> 'laravel senior developer',
-             'tags'=> 'laravel,javascript',
-             'company'=> 'laracorp',
-             'location'=> 'Lagos,lagos',
-             'email'=> 'email@email.com',
-             'website'=> 'https://www.laracorp.com',
-             'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
-             Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
-         ]);
+         Listing::factory(5)->create();
+        //  Listing::create([
+        //      'title'=> 'laravel senior developer',
+        //      'tags'=> 'laravel,javascript',
+        //      'company'=> 'laracorp',
+        //      'location'=> 'Lagos,lagos',
+        //      'email'=> 'email@email.com',
+        //      'website'=> 'https://www.laracorp.com',
+        //      'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
+        //      Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
+        //  ]);
 
-         Listing::create([
-            'title'=> 'laravel full-stack developer',
-            'tags'=> 'laravel,javascript',
-            'company'=> 'laracorp',
-            'location'=> 'Lagos,lagos',
-            'email'=> 'email@email.com',
-            'website'=> 'https://www.laracorp.com',
-            'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
-            Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
-        ]);
+        //  Listing::create([
+        //     'title'=> 'laravel full-stack developer',
+        //     'tags'=> 'laravel,javascript',
+        //     'company'=> 'laracorp',
+        //     'location'=> 'Lagos,lagos',
+        //     'email'=> 'email@email.com',
+        //     'website'=> 'https://www.laracorp.com',
+        //     'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
+        //     Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
+        // ]);
 
        // \App\Models\User::factory()->create([
        //      'name' => 'Test User',

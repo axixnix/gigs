9d3d22f1bd88a7ac9d9b297f10d3dae8ab0997fb
B <henryirons@yahoo.com>
2022-07-19 03:45:40 +0100

    used database seeder to seed two listings into the listings table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(8)->create();
+         Listing::create([
+             'title'=> 'laravel senior developer',
+             'tags'=> 'laravel,javascript',
+             'company'=> 'laracorp',
+             'location'=> 'Lagos,lagos',
+             'email'=> 'email@email.com',
+             'website'=> 'https://www.laracorp.com',
+             'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
+             Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         Listing::create([
+            'title'=> 'laravel full-stack developer',
+            'tags'=> 'laravel,javascript',
+            'company'=> 'laracorp',
+            'location'=> 'Lagos,lagos',
+            'email'=> 'email@email.com',
+            'website'=> 'https://www.laracorp.com',
+            'description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Tortor at risus viverra adipiscing at in tellus integer. Nisl tincidunt eget nullam non nisi est. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Eget nulla facilisi etiam dignissim. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Eu mi bibendum neque egestas congue. Commodo odio aenean sed adipiscing diam. Vitae congue mauris rhoncus aenean vel elit scelerisque. At tempor commodo ullamcorper a.
+            Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.',
+        ]);
+
+       // \App\Models\User::factory()->create([
+       //      'name' => 'Test User',
+       //     'email' => 'test@example.com',
+       //  ]);
     }
 }

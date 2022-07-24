bd06aa1c9c6eac48936fd1687a4d5436dab5ec9c
B <henryirons@yahoo.com>
2022-07-24 06:02:17 +0100

    created the listing-card component and used it in listings view

---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,43 @@
+{{-- @props() takes an array of props --}}
+@props(['listing'])
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src={{ asset('images/no-image.png') }}
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
+                {{--  test above says $listing is not a collection but this syntax is still working don't know what is going on here, spent almost an hour and it just started working without me making any changes, maybe i made a typo initially --}}
+            </h3>
+            <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+            <ul class="flex">
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Laravel</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">API</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Backend</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Vue</a>
+                </li>
+            </ul>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
+            </div>
+        </div>
+    </div>
+</div>

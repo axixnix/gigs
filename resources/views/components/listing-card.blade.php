ca31671a6ec8cd254481867b22884e157620ca25
B <henryirons@yahoo.com>
2022-07-25 06:21:17 +0100

    created card component, used it as a wrapper for the contents of the listing-card component

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,6 +1,7 @@
 {{-- @props() takes an array of props --}}
 @props(['listing'])
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
+
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
@@ -40,4 +41,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>

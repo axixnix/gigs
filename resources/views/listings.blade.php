fbf34c8c947872c34bd89e2daeb8b6b9a204cc81
B <henryirons@yahoo.com>
2022-09-21 12:20:06 +0100

    brought in the grid div and first item from the template into listings view

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -26,7 +26,7 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
 
         <p>
             {{ $listing['description'] }}
-        </p> --}}
+        </p>
         @php
 
            // dd($listing['title']);
@@ -35,48 +35,49 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             }else{
                 dd('not a collection');
             } */
-        @endphp
-
-        <div class="bg-gray-50 border border-gray-200 rounded p-6">
-            <div class="flex">
-                <img
-                    class="hidden w-48 mr-6 md:block"
-                    src={{ asset('images/no-image.png') }}
-                    alt=""
-                />
-                <div>
-                    <h3 class="text-2xl">
-                        <a href="show.html">{{ $listing->title }}</a>{{--  test above says $listing is not a collection but this syntax is still working don't know what is going on here, spent almost an hour and it just started working without me making any changes, maybe i made a typo initially --}}
-                    </h3>
-                    <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
-                    <ul class="flex">
-                        <li
-                            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                        >
-                            <a href="#">Laravel</a>
-                        </li>
-                        <li
-                            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                        >
-                            <a href="#">API</a>
-                        </li>
-                        <li
-                            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                        >
-                            <a href="#">Backend</a>
-                        </li>
-                        <li
-                            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                        >
-                            <a href="#">Vue</a>
-                        </li>
-                    </ul>
-                    <div class="text-lg mt-4">
-                        <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
-                    </div>
-                </div>
+        @endphp --}}
+<!-- Item 1 -->
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="images/acme.png"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="show.html">Senior Laravel Developer</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">Acme Corp</div>
+            <ul class="flex">
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Laravel</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">API</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Backend</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Vue</a>
+                </li>
+            </ul>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> Boston,
+                MA
             </div>
         </div>
+    </div>
+</div>
     @endforeach
 @else
     <p>no listing found</p>

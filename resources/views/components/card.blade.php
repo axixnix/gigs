8ed5cd836793fba955d21215e19f7675f4b77c28
B <henryirons@yahoo.com>
2022-12-06 00:35:53 +0100

    used attributes merge in card component so that we would be able to add classes to the card component wherever it is used

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<div {{ attributes->merge(['class'=>"bg-gray-50 border border-gray-200 rounded p-6"]) }} >{{-- this div will use these classes by default and also merge any classes added to the component where it is used --}}
 {{ $slot }}
 </div>
--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,6 +1,6 @@
 @props(['listing'])
 
-<x-card>
+<x-card class="p-10">{{-- remember this class here works because of the attributes merge we did in the card component --}}
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"

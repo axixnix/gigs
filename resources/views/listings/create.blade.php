adbf5a9f03a9147c78797aea614e858d40336df8
B <henryirons@yahoo.com>
2022-12-13 22:46:08 +0100

    field values remain if a user has an error after submitting a gig

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -21,6 +21,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value="{{ old('company') }}"
                             />
                             @error('company')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -37,6 +38,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{ old('title') }}"
                             />
                             @error('title')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -55,6 +57,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
+                                value="{{ old('location') }}"
                             />
                             @error('location')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -70,6 +73,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value="{{ old('email') }}"
                             />
                             @error('email')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -88,6 +92,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value="{{ old('website') }}"
                             />
                             @error('website')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -104,6 +109,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                                value="{{ old('tags') }}"
                             />
                             @error('tags')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -119,6 +125,7 @@
                                 type="file"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="logo"
+                                value="{{ old('logo') }}"
                             />@error('logo')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
 
@@ -137,6 +144,7 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
+                                value="{{ old('description') }}"
                             ></textarea>
                             @error('description')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
